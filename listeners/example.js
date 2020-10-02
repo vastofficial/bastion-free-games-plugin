1781b4f4bc8ad00fe3f6b3ba2dd31172a38d7c41
Vast Gaming <37594687+vastofficial@users.noreply.github.com>
2020-10-03 01:05:51 +0200

    Update example.js

--- listeners/example.js
+++ listeners/example.js
@@ -1,12 +1,13 @@
 /*!
- * @plugin bastion-example-plugin
- * @author Sankarsan Kampa (k3rn31p4nic)
+ * @plugin bastion-free-games-plugin
+ * @author Christoph Szabo (Vast)
  * @license MIT
  */
 
 "use strict";
 
 const tesseract = require("@bastion/tesseract");
+const rss = `https://steamcommunity.com/groups/freegamesfinders/rss/`;
 
 class ExampleListenerPlugin extends tesseract.Listener {
     constructor() {
@@ -14,7 +15,29 @@ class ExampleListenerPlugin extends tesseract.Listener {
         // Refer https://bastion.gitbook.io/docs/plugins#supported-events for all supported events.
         super("message");
     }
-
+    
+    fetch(rss)
+    .then(response => response.text())
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then(data => {
+        console.log(data);
+        const items = data.querySelectorAll("item");
+        let html = ``;
+        items.forEach(el => {
+            html += `
+                <article>
+                    <img src="${el.querySelector("link").innerHTML}/image/large.png" alt="">
+                <h2>
+                <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
+                ${el.querySelector("title").innerHTML}
+                </a>
+            </h2>
+            </article>
+        `;
+        });
+        document.body.insertAdjacentHTML("beforeend", html);
+    });
+    
     exec = async (message) => {
         console.log(message);
     }
